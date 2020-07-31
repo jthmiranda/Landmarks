b77db9a427202c549e4d0a0a6f27aa56995e41e9
Jonathan Miranda <miranda.jth@gmail.com>
2020-07-31 17:46:32 -0600

    Section 2

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello SwiftUI")
+        Text("Tutle Rock")
+            .font(.title)
+            .foregroundColor(Color.green)
+            
     }
 }
 
