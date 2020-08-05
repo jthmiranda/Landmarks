57e8176aa99d0cc0c69a4ec8c7e30abd9d8819db
Jonathan Miranda <miranda.jth@gmail.com>
2020-08-04 19:41:52 -0600

    Creating a Badge View - section 1

---
+++ Landmarks/Badge.swift
@@ -0,0 +1,21 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by Jonathan Miranda on 8/4/20.
+//  Copyright © 2020 Jonathan Miranda. All rights reserved.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        Text("Badge")
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
