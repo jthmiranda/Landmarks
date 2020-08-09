694346d58c4684431f64645d4799f12cc2652e49
Jonathan Miranda <miranda.jth@gmail.com>
2020-08-08 22:24:33 -0600

    animate the effect of state changes

--- Landmarks/HikeView.swift
+++ Landmarks/HikeView.swift
@@ -27,14 +27,16 @@ struct HikeView: View {
                 Spacer()
 
                 Button(action: {
-                    self.showDetail.toggle()
+                    withAnimation(.easeInOut(duration: 4)) {
+                        self.showDetail.toggle()
+                        
+                     }
                 }) {
                     Image(systemName: "chevron.right.circle")
                         .imageScale(.large)
                         .rotationEffect(.degrees(showDetail ? 90 : 0))
                         .scaleEffect(showDetail ? 1.5 : 1)
                         .padding()
-                        .animation(.spring())
                 }
             }
 
