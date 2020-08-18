80a37f4f5e7478dad3cccf1f8a15b972d65f11b8
Jonathan Miranda <miranda.jth@gmail.com>
2020-08-18 12:53:24 -0600

    Create the View Controller's Data Source - section 2

--- Landmarks/PageViewController.swift
+++ Landmarks/PageViewController.swift
@@ -13,11 +13,16 @@ struct PageViewController: UIViewControllerRepresentable {
     
     var controllers: [UIViewController]
     
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
     func makeUIViewController(context: Context) -> UIPageViewController {
         let pageViewController = UIPageViewController(
             transitionStyle: .scroll,
             navigationOrientation: .horizontal
         )
+        pageViewController.dataSource = context.coordinator
         
         return pageViewController
     }
@@ -26,5 +31,41 @@ struct PageViewController: UIViewControllerRepresentable {
     func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
         pageViewController.setViewControllers([controllers[0]], direction: .forward, animated: true)
     }
+    
+    class Coordinator: NSObject, UIPageViewControllerDataSource {
+        var parent: PageViewController
+        
+        init(_ parent: PageViewController) {
+            self.parent = parent
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+            guard let index = parent.controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            
+            if index == 0 {
+                return parent.controllers.last
+            }
+            return parent.controllers[index - 1]
+        }
+        
+        func pageViewController(_ pageViewControler: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+            guard let index = parent.controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            
+            if index + 1 == parent.controllers.count {
+                return parent.controllers.first
+            }
+            return parent.controllers[index + 1]
+        }
+    }
 }
 
+
+struct PageViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
