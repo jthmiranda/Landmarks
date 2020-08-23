ff336b5f41fce77436332a63a81663e6c85a5903
Jonathan Miranda <miranda.jth@gmail.com>
2020-08-22 21:11:11 -0600

    Adding a macOS target to the project
    changing watchOS version to 6 (Downgrading for compile time but need to recreate target project again :(  rekt )

---
+++ MacLandmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MacLandmarks
+//
+//  Created by Jonathan Miranda on 8/22/20.
+//  Copyright © 2020 Jonathan Miranda. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
