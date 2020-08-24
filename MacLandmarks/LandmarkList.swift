461189572a2f02af50139fee4664ce574cda9cb6
Jonathan Miranda <miranda.jth@gmail.com>
2020-08-24 11:26:21 -0600

    Creating a filter to manage the list

--- MacLandmarks/LandmarkList.swift
+++ MacLandmarks/LandmarkList.swift
@@ -11,13 +11,17 @@ import SwiftUI
 struct LandmarkList: View {
     @EnvironmentObject var userData: UserData
     @Binding var selecteLandmark: Landmark?
+    @Binding var filter: FilterType
     
     var body: some View {
         List(selection: $selecteLandmark) {
             ForEach(userData.landmarks) { landmark in
-                if !self.userData.showOnlyFavotiresOnly || landmark.isFavorite {
+                if (!self.userData.showOnlyFavotiresOnly || landmark.isFavorite)
+                    && (self.filter == .all
+                        || self.filter.category == landmark.category
+                        || (self.filter.category == .featured && landmark.isFeatured)) {
                     LandmarkRow(landmark: landmark).tag(landmark)
-                } 
+                }
             }
         }
     }
@@ -25,7 +29,7 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList(selecteLandmark: .constant(landmarkData[0]))
+        LandmarkList(selecteLandmark: .constant(landmarkData[0]), filter: .constant(.all))
         .environmentObject(UserData())
     }
 }
