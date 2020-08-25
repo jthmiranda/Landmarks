ff40752f4dbdd29abc39a4047c05a35af137630e
Jonathan Miranda <miranda.jth@gmail.com>
2020-08-24 18:48:47 -0600

    Extend the Map View for MacOS

--- Landmarks/Supporting Views/MapView.swift
+++ Landmarks/Supporting Views/MapView.swift
@@ -9,21 +9,50 @@
 import SwiftUI
 import MapKit
 
-struct MapView: UIViewRepresentable {
+struct MapView {
     var coordinate: CLLocationCoordinate2D
     
-    func makeUIView(context: Context) -> MKMapView {
-        return MKMapView(frame: .zero)
+    func makeMapView() -> MKMapView {
+        MKMapView(frame: .zero)
     }
     
-    func updateUIView(_ uiView: MKMapView, context: Context) {
-//        let coordinate = CLLocationCoordinate2D(latitude: 34.011286, longitude: -116.166868)
+    func updateMapView(_ uiView: MKMapView, context: Context) {
         let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
         let region = MKCoordinateRegion(center: coordinate, span: span)
         
         uiView.setRegion(region, animated: true)
     }
 }
+
+#if os(macOS)
+
+extension MapView: NSViewRepresentable {
+        
+    func makeNSView(context: Context) -> MKMapView {
+        makeMapView()
+    }
+    
+    func updateNSView(_ nsView: MKMapView, context: Context) {
+        updateMapView(nsView, context: context)
+    }
+    
+}
+
+#else
+
+extension MapView: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> MKMapView {
+        makeMapView()
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        updateMapView(uiView, context: context)
+    }
+    
+}
+
+#endif
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
