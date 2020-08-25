c22a561e3be681f526b1960b728aaee19a04d3b2
Jonathan Miranda <miranda.jth@gmail.com>
2020-08-24 18:35:34 -0600

    preparing the CircleImage to reuse

--- Landmarks/Supporting Views/CircleImage.swift
+++ Landmarks/Supporting Views/CircleImage.swift
@@ -10,12 +10,13 @@ import SwiftUI
 
 struct CircleImage: View {
     var image: Image
+    var shadowRadius: CGFloat = 10
     
     var body: some View {
         image
             .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
             .overlay(Circle().stroke(Color.white, lineWidth: 4))
-            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            .shadow(radius: shadowRadius)
     }
 }
 
